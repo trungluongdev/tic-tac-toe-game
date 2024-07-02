0da441745ff12c167fcb3423bc479f5675478b20
trungluongdev <chitrungluong.cv@gmail.com>
2024-07-02 15:13:30 +0700

    completed project

---
+++ src/components/History.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function History({ history, jumpTo }) {
+    return (
+        <div className="history">
+            <h4>History</h4>
+            <ul>
+                {history.map((step, move) => {
+                    const desc = move ? "Go to move #" + move : "Go to game start";
+                    return (
+                        <li key={move}>
+                            <button onClick={() => jumpTo(move)}>{desc}</button>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default History;
